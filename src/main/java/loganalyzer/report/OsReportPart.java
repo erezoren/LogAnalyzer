a0aecc5611233a55bc9442314b6ec44f7f8e0277
erezoren <erez_oren@yahoo.com>
2025-08-08 16:13:36 +0300

    abstraction

--- src/main/java/loganalyzer/report/OsReportPart.java
+++ src/main/java/loganalyzer/report/OsReportPart.java
@@ -1,15 +1,11 @@
 package loganalyzer.report;
 
-import java.util.Comparator;
-import java.util.List;
 import java.util.Map;
 
 public class OsReportPart extends ReportPart {
 
-  final Map<String, Integer> rawReport;
-
   public OsReportPart(Map<String, Integer> rawReport) {
-    this.rawReport = rawReport;
+    super(rawReport);
   }
 
   @Override
@@ -17,16 +13,5 @@ public class OsReportPart extends ReportPart {
     return "Operating System";
   }
 
-  @Override
-  public List<ReportValue> getReport() {
-    int totalOperatingSystems = rawReport.values().stream().mapToInt(Integer::intValue).sum();
 
-    List<ReportValue> reportValues = rawReport.entrySet().stream().map(es -> {
-      double precent = (double) es.getValue() / totalOperatingSystems;
-      double rounded = Math.round(precent * 100.0) / 100.0;
-      return new ReportValue(es.getKey(), rounded);
-    }).sorted(Comparator.comparingDouble((ReportValue r) -> r.percentage).reversed()).toList();
-
-    return reportValues;
-  }
 }
--- src/main/java/loganalyzer/report/ReportPart.java
+++ src/main/java/loganalyzer/report/ReportPart.java
@@ -1,18 +1,34 @@
 package loganalyzer.report;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 public abstract class ReportPart {
 
   private final String bold = "\033[1m";
   private final String reset = "\033[0m";
+  final Map<String, Integer> rawReport;
+
+  protected ReportPart(Map<String, Integer> rawReport) {
+    this.rawReport = rawReport;
+  }
 
   abstract String getName();
-
-  abstract List<ReportValue> getReport();
 
   public void print() {
     System.out.println(String.format(bold + "%s:", getName() + reset));
     getReport().forEach(rv -> System.out.println(rv.toString()));
+  }
+
+  public List<ReportValue> getReport() {
+    int totalOperatingSystems = rawReport.values().stream().mapToInt(Integer::intValue).sum();
+    List<ReportValue> reportValues = rawReport.entrySet().stream().map(es -> {
+      double precent = (double) es.getValue() / totalOperatingSystems;
+      double rounded = Math.round(precent * 100.0) / 100.0;
+      return new ReportValue(es.getKey(), rounded);
+    }).sorted(Comparator.comparingDouble((ReportValue r) -> r.percentage).reversed()).toList();
+
+    return reportValues;
   }
 }
